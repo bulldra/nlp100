42221b58431f8d23f295fcb484a14e0ad5b01adf
bulldra <bullda@gmail.com>
2021-06-18 10:13:28 +0900

    requirements.txt整理

--- test.sh
+++ test.sh
@@ -1,2 +1,9 @@
-docker-compose run --entrypoint "pytest -s ./tests/" nlp100
+#!/bin/bash
+
+if [ $# == 1 ]; then
+    TEST_PATH="./tests/$1"
+else
+    TEST_PATH="./tests/"
+fi
+docker-compose run --entrypoint "pytest -s $TEST_PATH" nlp100
 docker-compose down
